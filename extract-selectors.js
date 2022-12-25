1ebda80fa6ee283469562c431373c6f71a4b02b3
DarkArtek <luca@ahd-creative.agency>
2022-12-25 07:43:49 +0100

    Initial Commit

---
+++ extract-selectors.js
@@ -0,0 +1,23 @@
+const listSelectors = require('list-selectors');
+const fs = require('fs');
+const { exec } = require('child_process');
+
+process.env.NODE_ENV = 'production';
+
+exec('tailwind build src/tailwind.css > build/tw.css', {}, () => {
+    listSelectors(
+        ['./build/tw.css'],
+        { include: ['classes'] },
+        ({ classes }) => {
+            fs.writeFileSync(
+                './build/usedCSSClasses.json',
+                JSON.stringify(
+                    classes.map((c) => c
+                        .substring(1)
+                        .split('\\')
+                        .join('')),
+                ),
+            );
+        },
+    );
+});
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: {
+        content: [
+            './src/**/*.{html,js,jsx,ts,tsx}',
+        ],
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: { extend: { boxShadow: { 'inner-sm': 'inset 0 1px 2px 0 rgba(0, 0, 0, 0.05)' } } },
+    plugins: [
+        require('@ahdcreative/tailwind-config-ffxivita'),
+    ],
+};
